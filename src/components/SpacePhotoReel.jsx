0e498556d33f0b9c61996d6a84e7426799884625
daksh <dakshhardiya34@gmail.com>
2025-10-27 23:17:26 +0530

    adding the photo part

---
+++ src/components/SpacePhotoReel.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {motion} from "framer-motion"
+import img1 from "../assets/1.jpg";
+import img2 from "../assets/2.jpg";
+import img3 from "../assets/3.jpg";
+import img4 from "../assets/4.jpg";
+import img5 from "../assets/5.jpg";
+
+const images = [img1, img2,img3, img4, img5];
+
+const SpacePhotoReel=()=>{
+    return(
+        <div></div>
+    )
+}
